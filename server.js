18874f65ecdb7588407a86fe35d709bca7561722
rishabh <rish6696.rs@gmail.com>
2019-09-07 19:26:05 +0530

    insatruction in server.js for pm2 babel run

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
  
 
-mongoose.connect(config.DB_URL)
+mongoose.connect(config.DB_URL,)
 .then(x=>console.log('mongodb connected successfully'))
 .catch(e=>console.log(e))
 
@@ -18,3 +18,7 @@ app.use('/v1',route)
 app.use('/',(req,res)=>res.send('hello'))
 
 app.listen(5896,x=>console.log(`server started`))
+
+//install babel-cli globally for pm2 
+//then run pm2 start npm -- run start
+
